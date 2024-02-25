57b2df20be26ad3d10c8e0e0ae4f134cbc528766
Aryan Ahmed <thearyanahmed@gmail.com>
2024-02-25 22:45:40 +0100

    Add pagination, resizable & sonner for windows

--- install-components.bat
+++ install-components.bat
@@ -2,7 +2,7 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 REM Components
-set components=accordion alert alert-dialog aspect-ratio avatar badge button calendar card checkbox collapsible combobox command context-menu data-table date-picker dialog dropdown-menu form hover-card input label menubar navigation-menu popover progress radio-group scroll-area select separator sheet skeleton slider switch table tabs textarea toast toggle tooltip
+set components=accordion alert alert-dialog aspect-ratio avatar badge button calendar card checkbox collapsible combobox command context-menu data-table date-picker dialog dropdown-menu form hover-card input label menubar navigation-menu popover progress pagination radio-group resizable sonner scroll-area select separator sheet skeleton slider switch table tabs textarea toast toggle tooltip
 
 REM Loop through each component and install it
 for %%c in (%components%) do (
